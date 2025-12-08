483e428402a915e2eacc630dc56e9bc5c8054102
Gus <yoldi.gustavo@gmail.com>
2025-12-08 10:58:44 -0300

    fix: unify javascript

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 'use strict';
 
-import setInitialPlayerNames from './features/playerName';
-
 //Selecting elements
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
@@ -102,3 +100,40 @@ btnNew.addEventListener('click', () => {
 btnChangeName.addEventListener('click', () => {
   promptPlayerNames();
 });
+
+//Player name functions
+function setInitialPlayerNames() {
+  const defaultName0 = 'Jugador 1';
+  const defaultName1 = 'Jugador 2';
+  const [name0, name1] = getPlayerName();
+
+  if (!name0 || !name1) {
+    localStorage.setItem('player0Name', defaultName0);
+    localStorage.setItem('player1Name', defaultName1);
+
+    player0El.querySelector('.name').innerHTML = name0;
+    player1El.querySelector('.name').innerHTML = name1;
+  }
+
+  player0El.querySelector('.name').innerHTML = name0 || defaultName0;
+  player1El.querySelector('.name').innerHTML = name1 || defaultName1;
+}
+
+function promptPlayerNames() {
+  const player0Name = prompt('Nombre del Jugador 1:') ?? 'Jugador 1';
+  const player1Name = prompt('Nombre del Jugador 2:') ?? 'Jugador 2';
+
+  localStorage.setItem('player0Name', player0Name);
+  localStorage.setItem('player1Name', player1Name);
+
+  const [name0, name1] = getPlayerName();
+  player0El.querySelector('.name').innerHTML = name0;
+  player1El.querySelector('.name').innerHTML = name1;
+}
+
+function getPlayerName() {
+  const name0 = localStorage.getItem('player0Name');
+  const name1 = localStorage.getItem('player1Name');
+
+  return [name0, name1];
+}
